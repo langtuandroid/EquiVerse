373df5267b0b0f4662944791f29214d8a7fa91b6
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2023-12-15 19:47:53 +0100

    Created newcompanion scene.

--- Assets/RandomIdleController.cs
+++ Assets/RandomIdleController.cs
@@ -8,17 +8,20 @@ public class RandomIdleController : MonoBehaviour
 
     public GameObject eggPrefab;
 
+    public float secondIdleTriggerMinWait = 5f;
+    public float secondIdleTriggerMaxWait = 15f;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-        Invoke("TriggerSecondIdle", Random.Range(5f, 15f));
+        Invoke("TriggerSecondIdle", Random.Range(secondIdleTriggerMinWait, secondIdleTriggerMaxWait));
         Invoke("EggDropTrigger", Random.Range(15f, 45f));
     }
 
     void TriggerSecondIdle()
     {
         animator.SetTrigger("SecondIdleTrigger");
-        Invoke("TriggerSecondIdle", Random.Range(5f, 15f));
+        Invoke("TriggerSecondIdle", Random.Range(secondIdleTriggerMinWait, secondIdleTriggerMaxWait));
     }
 
     void EggDropTrigger()
