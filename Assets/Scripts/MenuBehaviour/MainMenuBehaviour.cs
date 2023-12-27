d759a784a55712272cc79825b61b3b97ad321dc0
DESKTOP-9M2UNS6\Manfred Mosterdfiets <wiavre@gmail.com>
2023-12-27 12:59:47 +0100

    Added contintue button in main menu and new adventure button

--- Assets/Scripts/MenuBehaviour/MainMenuBehaviour.cs
+++ Assets/Scripts/MenuBehaviour/MainMenuBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using DG.Tweening;
 using Managers;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -16,6 +17,9 @@ namespace MainMenu
         [Header("ToggleMenu's")] 
         public GameObject mainMenu;
         public GameObject optionsMenu;
+
+        public GameObject continueAdventureObject;
+        public TextMeshProUGUI continueAdventureButton;
         
         private void Start()
         {
@@ -24,10 +28,26 @@ namespace MainMenu
                 GameManager.WORLD_INDEX = 1;
                 GameManager.LEVEL_INDEX = 1;
                 GameManager.firstTimePlaying = false;
+                continueAdventureObject.SetActive(false);
+            }
+            else
+            {
+                continueAdventureObject.SetActive(true);
+                continueAdventureButton.text = "Continue adventure (" + GameManager.WORLD_INDEX.ToString() + "-" + GameManager.LEVEL_INDEX.ToString() + ")";
             }
         }
 
-        public void ClickPlay()
+        public void ClickNew()
+        {
+            GameManager.WORLD_INDEX = 1;
+            GameManager.LEVEL_INDEX = 1;
+            transitionOverlay.DOFade(1f, 1.2f).SetEase(Ease.InCubic).OnComplete((() =>
+            {
+                StartCoroutine(LoadAsynchronously("Level " + GameManager.WORLD_INDEX.ToString() + "-" + GameManager.LEVEL_INDEX.ToString()));
+            }));
+        }
+        
+        public void ClickContinue()
         {
             transitionOverlay.DOFade(1f, 1.2f).SetEase(Ease.InCubic).OnComplete((() =>
             {
