894a827cd527e5cf0538370fa94d142b69389887
Erwin van Crasbeek <evancrasbeek@xs4all.nl>
2024-01-02 13:26:15 +0100

    Fixed total points lerping issues.

--- Assets/Scripts/PointSystem/ECManager.cs
+++ Assets/Scripts/PointSystem/ECManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using DG.Tweening;
 using TMPro;
@@ -6,14 +7,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Managers
-{
-    public class ECManager : MonoBehaviour
-    {
+namespace Managers {
+    public class ECManager : MonoBehaviour {
         public TextMeshProUGUI totalPointsText;
         public float lerpDuration = 0.5f;
         public Slider slider;
-        public static int totalPoints;
+        public static int totalPoints, visualPoints;
         public int startingPoints;
         public int lowValuePoints;
         public int gooseEggPoints;
@@ -24,103 +23,108 @@ namespace Managers
         public GameObject loadingScreen;
         public GuidedTutorial guidedTutorialManager;
 
-        private Coroutine lerpCoroutine;
+        private DateTime lastVisualUpdate = DateTime.Now;
+        private const float updateInterval = 0.01666667f;
 
-        private void Start()
-        {
+        private void Start() {
             totalPoints = startingPoints;
+            visualPoints = totalPoints;
             UpdatePointText();
         }
 
-        private void FixedUpdate()
-        {
-            totalPointsText.text = totalPoints.ToString();
-        }
+        //private void FixedUpdate() {
+        //    totalPointsText.text = totalPoints.ToString();
+        //}
 
-        public void AddLowValuePoints()
-        {
+        public void AddLowValuePoints() {
             IncrementPoints(lowValuePoints);
         }
 
-        public void AddGooseEggPoints()
-        {
+        public void AddGooseEggPoints() {
             IncrementPoints(gooseEggPoints);
         }
 
-        public void BuyEndOfLevel()
-        {
-            if (totalPoints >= endOfLevelCost)
-            {
-                if ((int)slider.value == (int)slider.maxValue)
-                {
+        public void BuyEndOfLevel() {
+            if (totalPoints >= endOfLevelCost) {
+                if ((int)slider.value == (int)slider.maxValue) {
                     LevelCompleted();
-                }
-                else
-                {
+                } else {
                     DecrementPoints(endOfLevelCost);
                     slider.value += 1;
                 }
             }
         }
 
-        IEnumerator LerpPoints(int startPoints, int targetPoints)
-        {
-            float elapsedTime = 0f;
+        public void Update() {
+            UpdateVisualPoints();
+        }
 
-            while (elapsedTime < lerpDuration)
-            {
-                totalPoints = Mathf.RoundToInt(Mathf.Lerp(startPoints, targetPoints, elapsedTime / lerpDuration));
+
+        public void UpdateVisualPoints() {
+            DateTime now = DateTime.Now;
+            if ((now - lastVisualUpdate).TotalSeconds > updateInterval) {
+                if (visualPoints < totalPoints) visualPoints++;
+                else if (visualPoints > totalPoints) visualPoints--;
+                lastVisualUpdate = now;
                 UpdatePointText();
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-
-            totalPoints = targetPoints;
-            UpdatePointText();
-            lerpCoroutine = null; // Set coroutine to null when finished
-        }
-
-        void UpdatePointText()
-        {
-            if (totalPointsText != null)
-            {
-                totalPointsText.text = totalPoints.ToString();
             }
         }
 
-        public void IncrementPoints(int amount)
-        {
-            if (lerpCoroutine != null)
-            {
-                StopCoroutine(lerpCoroutine); // Stop the current coroutine if it's running
-            }
 
-            int newPoints = totalPoints + amount;
-            lerpCoroutine = StartCoroutine(LerpPoints(totalPoints, newPoints));
+        //IEnumerator LerpPoints(int startPoints, int targetPoints)
+        //{
+        //    float elapsedTime = 0f;
+        //
+        //    while (elapsedTime < lerpDuration)
+        //    {
+        //        totalPoints = Mathf.RoundToInt(Mathf.Lerp(startPoints, targetPoints, elapsedTime / lerpDuration));
+        //        UpdatePointText();
+        //        elapsedTime += Time.deltaTime;
+        //        yield return null;
+        //    }
+        //
+        //    totalPoints = targetPoints;
+        //    UpdatePointText();
+        //    lerpCoroutine = null; // Set coroutine to null when finished
+        //}
+
+        void UpdatePointText() {
+            if (totalPointsText != null) {
+                totalPointsText.text = visualPoints.ToString();
+            }
         }
 
-        public void DecrementPoints(int amount)
-        {
-            if (lerpCoroutine != null)
-            {
-                StopCoroutine(lerpCoroutine); // Stop the current coroutine if it's running
-            }
-
-            int newPoints = totalPoints - amount;
-            lerpCoroutine = StartCoroutine(LerpPoints(totalPoints, newPoints));
+        public void IncrementPoints(int amount) {
+            totalPoints += amount;
+            //if (lerpCoroutine != null)
+            //{
+            //    StopCoroutine(lerpCoroutine); // Stop the current coroutine if it's running
+            //}
+            //
+            //int newPoints = totalPoints + amount;
+            //lerpCoroutine = StartCoroutine(LerpPoints(totalPoints, newPoints));
         }
 
-        private void LevelCompleted()
-        {
-            transitionOverlay.DOFade(1f, 1.2f).SetEase(Ease.InCubic).OnComplete((() =>
-            {
+        public void DecrementPoints(int amount) {
+            totalPoints -= amount;
+
+            //if (lerpCoroutine != null)
+            //{
+            //    StopCoroutine(lerpCoroutine); // Stop the current coroutine if it's running
+            //}
+            //
+            //int newPoints = totalPoints - amount;
+            //lerpCoroutine = StartCoroutine(LerpPoints(totalPoints, newPoints));
+        }
+
+        private void LevelCompleted() {
+            transitionOverlay.DOFade(1f, 1.2f).SetEase(Ease.InCubic).OnComplete((() => {
                 guidedTutorialManager.enabled = false;
                 StartCoroutine(LoadAsynchronously("NewCompanionScene"));
             })).SetUpdate(true);
         }
 
-        IEnumerator LoadAsynchronously(string sceneIndex)
-        {
+        IEnumerator LoadAsynchronously(string sceneIndex) {
             loadingScreen.SetActive(true);
             AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
             yield return operation;
