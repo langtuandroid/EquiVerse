b0f3e5a53a0eedfbbc77273538e1b8e858dcd60d
w113083i <wiavre@gmail.com>
2024-01-08 23:03:46 +0100

    Improved NewCompanion visual feedback.

--- Assets/Scripts/MenuBehaviour/NewCompanion/CompanionRevealer.cs
+++ Assets/Scripts/MenuBehaviour/NewCompanion/CompanionRevealer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -8,13 +9,35 @@ public class CompanionRevealer : MonoBehaviour
 {
     public Button giftButton;
     public RectTransform companion;
+    public Button nextLevelButton;
+
+    private void Start()
+    {
+        DOVirtual.DelayedCall(2.0f, () =>
+            {
+                giftButton.transform.DOShakePosition(1.0f, new Vector3(5, 5, 0), 10, 90, false, true);
+            })
+            .SetLoops(-1, LoopType.Restart);
+    }
 
     public void RevealCompanion()
     {
-        giftButton.transform.DOScale(0, 1.5f).SetEase(Ease.InBack).OnComplete((() =>
-        {
-            companion.gameObject.SetActive(true);
-            companion.DOSizeDelta(new Vector2(750, 800), 1.5f).SetEase(Ease.OutBack);
-        }));
+        giftButton.transform.DOScale(0, 2.5f)
+            .SetEase(Ease.InBack)
+            .OnPlay(() =>
+            {
+                // Rotate the giftButton during the scale tween
+                giftButton.transform.DORotate(new Vector3(0, 0, 900), 2.5f, RotateMode.FastBeyond360)
+                    .SetEase(Ease.InCubic)
+                    .SetLoops(-1, LoopType.Incremental);
+            })
+            .OnComplete(() =>
+            {
+                companion.gameObject.SetActive(true);
+                companion.DOSizeDelta(new Vector2(750, 800), 2f).SetEase(Ease.OutBack).OnComplete((() =>
+                {
+                    nextLevelButton.gameObject.SetActive(true);
+                }));
+            });
     }
 }
