bdedf6746a00d2ebfd46ce940caeda847644265c
w113083i <wiavre@gmail.com>
2024-01-15 17:25:13 +0100

    SwampGolem now has a death explosion.

--- Assets/Scripts/ClickSystem/Enemy/EnemyHealth.cs
+++ Assets/Scripts/ClickSystem/Enemy/EnemyHealth.cs
@@ -8,6 +8,7 @@ public class EnemyHealth : Clickable
 {
     public int enemyHealth;
     public GameObject reward;
+    public GameObject deathParticles;
     public override void OnClick()
     {
         if (enemyHealth > GunBehaviour.gunDamage)
@@ -23,8 +24,10 @@ public class EnemyHealth : Clickable
     private void Die()
     {
         transform.DOScale(0, 1f).SetEase(Ease.OutExpo);
+        Instantiate(deathParticles, gameObject.transform.position, Quaternion.identity);
         Instantiate(reward, gameObject.transform.position , Quaternion.identity);
         
         Destroy(gameObject, 1f);
+        Destroy(deathParticles, 5f);
     }
 }
