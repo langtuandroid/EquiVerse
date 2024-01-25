fc1a04be6ac6a71d2fed280b29cc57988ddc54d9
w113083i <wiavre@gmail.com>
2024-01-25 12:55:30 +0100

    Fixed resolutionsdropdown and refreshrate dropdown

--- Assets/Scripts/MenuBehaviour/OptionsMenu.cs
+++ Assets/Scripts/MenuBehaviour/OptionsMenu.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
@@ -7,14 +6,13 @@ using System.Linq;
 public class OptionsMenu : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
-    public TMP_Dropdown refreshRateDropdown;
     public TMP_Dropdown qualityDropdown;
     public TMP_Dropdown screenModeDropdown;
+    public TMP_Dropdown frameRateDropdown;
 
     private List<Resolution> filteredResolutions;
     private Resolution[] resolutions;
 
-    private float currentRefreshRate;
     private int currentResolutionIndex = 0;
 
     void Start()
@@ -22,50 +20,53 @@ public class OptionsMenu : MonoBehaviour
         PopulateQualityDropdown();
         PopulateResolutionDropdown();
         PopulateScreenModeDropdown();
+        PopulateFrameRateDropdown();
 
         screenModeDropdown.onValueChanged.AddListener(OnScreenModeChanged);
+        resolutionDropdown.onValueChanged.AddListener(OnResolutionChanged);
+        qualityDropdown.onValueChanged.AddListener(OnQualityChanged);
+        frameRateDropdown.onValueChanged.AddListener(OnFrameRateChanged);
+
+        // Set the default resolution to the current resolution
+        SetDefaultResolution();
     }
 
     void PopulateResolutionDropdown()
     {
         resolutions = Screen.resolutions;
         filteredResolutions = new List<Resolution>();
-        HashSet<int> uniqueRefreshRates = new HashSet<int>();
+        HashSet<string> uniqueResolutions = new HashSet<string>();
 
         resolutionDropdown.ClearOptions();
-        refreshRateDropdown.ClearOptions(); 
-        currentRefreshRate = Screen.currentResolution.refreshRate;
+        currentResolutionIndex = FindCurrentResolutionIndex();
 
-        foreach (var res in resolutions) {
-            uniqueRefreshRates.Add(res.refreshRate);
-            if (res.refreshRate == currentRefreshRate) {
+        foreach (var res in resolutions)
+        {
+            string resolutionString = $"{res.width}x{res.height}";
+
+            if (uniqueResolutions.Add(resolutionString))
+            {
                 filteredResolutions.Add(res);
             }
         }
 
-        List<string> resolutionOptions = new List<string>();
-        for (int i = 0; i < filteredResolutions.Count; i++) {
-            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height;
-            resolutionOptions.Add(resolutionOption);
-            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height) {
-                currentResolutionIndex = i;
-            }
-        }
+        List<string> resolutionOptions = filteredResolutions.Select(res => $"{res.width}x{res.height}").ToList();
         resolutionDropdown.AddOptions(resolutionOptions);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
 
-        List<string> refreshRateOptions = new List<string>();
-        List<int> sortedRefreshRates = uniqueRefreshRates.ToList();
-        sortedRefreshRates.Sort();
+    void SetDefaultResolution()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+        string currentResolutionString = $"{currentResolution.width}x{currentResolution.height}";
+        int defaultIndex = filteredResolutions.FindIndex(res => $"{res.width}x{res.height}" == currentResolutionString);
 
-        foreach (int rate in sortedRefreshRates) {
-            refreshRateOptions.Add(rate + "Hz");
+        if (defaultIndex != -1)
+        {
+            resolutionDropdown.value = defaultIndex;
+            resolutionDropdown.RefreshShownValue();
         }
-
-        refreshRateDropdown.AddOptions(refreshRateOptions);
-        refreshRateDropdown.value = refreshRateDropdown.options.FindIndex(option => option.text == currentRefreshRate + "Hz");
-        refreshRateDropdown.RefreshShownValue();
     }
 
     void PopulateQualityDropdown()
@@ -93,6 +94,23 @@ public class OptionsMenu : MonoBehaviour
 
         screenModeDropdown.AddOptions(screenModeOptions);
         SetDropdownValueBasedOnCurrentScreenMode();
+    }
+
+    void PopulateFrameRateDropdown()
+    {
+        frameRateDropdown.ClearOptions();
+
+        List<string> frameRateOptions = new List<string>
+        {
+            "30 FPS",
+            "60 FPS",
+            "120 FPS"
+            // Add more options as needed
+        };
+
+        frameRateDropdown.AddOptions(frameRateOptions);
+        frameRateDropdown.value = frameRateOptions.IndexOf($"{Application.targetFrameRate} FPS");
+        frameRateDropdown.RefreshShownValue();
     }
 
     void OnScreenModeChanged(int index)
@@ -131,27 +149,40 @@ public class OptionsMenu : MonoBehaviour
         }
     }
 
-    public void ApplyChanges()
-    {
-        OnResolutionChanged(resolutionDropdown.value);
-        OnQualityChanged(qualityDropdown.value);
-    }
-
-    public void OnResolutionChanged(int index)
+    void OnResolutionChanged(int index)
     {
         Resolution selectedResolution = filteredResolutions[index];
-
-        string selectedRefreshRateString = refreshRateDropdown.options[refreshRateDropdown.value].text;
-        int selectedRefreshRate = int.Parse(selectedRefreshRateString.Replace("Hz", ""));
-
-
-        Screen.SetResolution(selectedResolution.width, selectedResolution.height, true, selectedRefreshRate );
+        Screen.SetResolution(selectedResolution.width, selectedResolution.height, true);
     }
 
-    public void OnQualityChanged(int index)
+    void OnQualityChanged(int index)
     {
         QualitySettings.SetQualityLevel(index);
     }
+
+    void OnFrameRateChanged(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Application.targetFrameRate = 30;
+                break;
+            case 1:
+                Application.targetFrameRate = 60;
+                break;
+            case 2:
+                Application.targetFrameRate = 120;
+                break;
+            // Add more options as needed
+            default:
+                break;
+        }
+    }
+
+    int FindCurrentResolutionIndex()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+        string currentResolutionString = $"{currentResolution.width}x{currentResolution.height}";
+        return filteredResolutions.FindIndex(res => $"{res.width}x{res.height}" == currentResolutionString);
+    }
 }
-
-
