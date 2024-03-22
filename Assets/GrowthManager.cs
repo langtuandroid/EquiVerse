91b8f929a3da04035be2f0b33624a38b0957f295
w113083i <wiavre@gmail.com>
2024-03-22 10:44:58 +0100

    Added soundeffect and particles when rabbit grows to next stage

--- Assets/GrowthManager.cs
+++ Assets/GrowthManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class GrowthManager : MonoBehaviour
 {
+    public ParticleSystem growthParticleSystem;
+    
     private int growthProgressValue;
     private int adolescentTreshold = 50;
     private int adultTreshold = 100;
@@ -26,12 +28,16 @@ public class GrowthManager : MonoBehaviour
     
         if (growthProgressValue >= adolescentTreshold && !isAdolescent)
         {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Animals/RabbitGrow");
+            growthParticleSystem.Play();
             transform.DOScale(0.75f, 1f).SetEase(Ease.OutElastic).SetDelay(0.5f);
             isAdolescent = true;
         }
     
         if (isAdolescent && growthProgressValue >= adultTreshold && !isAdult)
         {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Animals/RabbitGrow");
+            growthParticleSystem.Play();
             transform.DOScale(1.1f, 1f).SetEase(Ease.OutElastic).SetDelay(0.5f);
             isAdult = true;
         }
