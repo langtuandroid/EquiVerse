6da15e8625b51c74aacf2a64e2bc730de0880f67
w113083i <wiavre@gmail.com>
2024-04-12 11:06:56 +0200

    Refactored camerasystem to Cinemachine

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,83 +1,50 @@
+using Cinemachine;
 using UnityEngine;
 
 namespace Input
 {
     public class CameraMovement : MonoBehaviour
     {
-        public Transform target; // The object around which the camera rotates
-        public float rotationSpeed = 13f; // Speed of rotation
-        public float zoomSpeed = 13f;
-        public float maxDistance = 80f;
-        public float minDistance = 20f; // Added minimum zoom distance
         private bool movedLeft = false, movedRight = false, movedUp = false, movedDown = false;
         public bool CameraLocked { get; set; }
-        private new Camera camera;
         private bool cameraStepCompleted = false;
+
+        public CinemachineFreeLook mainCam;
 
         private void Start()
         {
-            camera = GetComponent<Camera>();
             CameraLocked = true;
+            mainCam.gameObject.SetActive(false);
         }
 
         private void Update()
         {
-            if (!CameraLocked)
+            if (!CameraLocked && !cameraStepCompleted)
             {
-                MoveCamera();
-                if (!cameraStepCompleted)
-                {
-                    CheckCameraTutorial(); 
-                }
+                CheckCameraInput();
+                CheckCameraTutorial();
             }
         }
 
-        private void MoveCamera()
+        private void CheckCameraInput()
         {
+            mainCam.gameObject.SetActive(true);
             if (UnityEngine.Input.GetKey(KeyCode.A))
             {
-                RotateCamera(1f); // Rotate left
                 movedLeft = true;
             }
             else if (UnityEngine.Input.GetKey(KeyCode.D))
             {
-                RotateCamera(-1f); // Rotate right
                 movedRight = true;
             }
-        
-            // Clamp zoom within the defined limits
-            camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, minDistance, maxDistance);
-
-            if (camera.fieldOfView <= maxDistance && camera.fieldOfView >= minDistance)
+            if (UnityEngine.Input.GetKey(KeyCode.W))
             {
-                if (UnityEngine.Input.GetKey(KeyCode.W))
-                {
-                    ZoomCamera(-1f);
-                    movedUp = true;
-                }
-                else if (UnityEngine.Input.GetKey(KeyCode.S))
-                {
-                    ZoomCamera(1f);
-                    movedDown = true;
-                }
+                movedUp = true;
             }
-        }
-    
-        private void RotateCamera(float direction)
-        {
-            Vector3 yAxis = Vector3.up; // Axis of rotation (vertical axis)
-
-            // Calculate the desired rotation angle based on the direction and speed
-            float angle = direction * rotationSpeed * Time.deltaTime;
-
-            // Rotate the camera around the target object
-            transform.RotateAround(target.position, yAxis, angle);
-        }
-
-        private void ZoomCamera(float direction)
-        {
-            float zoomDirection = direction * zoomSpeed * Time.deltaTime;
-            camera.fieldOfView += zoomDirection;
+            else if (UnityEngine.Input.GetKey(KeyCode.S))
+            {
+                movedDown = true;
+            }
         }
 
         private void CheckCameraTutorial()
