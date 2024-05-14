f46414a37e80b0601a6ab5f3da44e5870dc1e46e
w113083i <wiavre@gmail.com>
2024-05-14 11:36:14 +0200

    Can't buy effects play when not enough leafpoints for plant placement

--- Assets/Scripts/Spawners/FoodSpawner.cs
+++ Assets/Scripts/Spawners/FoodSpawner.cs
@@ -9,6 +9,7 @@ using UnityEngine.Serialization;
 
 namespace Spawners {
     public class FoodSpawner : MonoBehaviour {
+        
         [Header("Managers")]
         public GameManager gameManager;
         public LeafPointManager leafPointManager;
@@ -18,7 +19,8 @@ namespace Spawners {
         [HideInInspector]
         public int foodPrefabsIndex = 0;
 
-        [Header("GrassCost")] public int grassCost;
+        [Header("GrassCost")] 
+        public int grassCost;
 
         [Header("Maximum plants")]
         public int maxPlants = 2;
@@ -34,41 +36,55 @@ namespace Spawners {
             timesPopupShown = 0;
         }
 
-        public void ClickOnGround(Vector3 point) {
-            if (!CanSpawnPlants) return;
-            point = new Vector3(point.x, 0.5f, point.z); //Hackerman fix for plants spawning too high. Should work until there's a level where the ground has a different height.
-            if (IsPointOnNavMesh(point) && LeafPointManager.totalPoints >= grassCost) {
-                if (currentPlantCount < maxPlants)
+        public void ClickOnGround(Vector3 point) 
+        {
+            if (!CanSpawnPlants) 
+                return;
+
+            point = new Vector3(point.x, 0.5f, point.z); // Adjust spawn height to prevent plants from spawning too high
+
+            if (IsPointOnNavMesh(point)) 
+            {
+                if (LeafPointManager.totalPoints >= grassCost) 
                 {
-                    GameObject spawnedPrefab = Instantiate(foodPrefabs[foodPrefabsIndex], point, Quaternion.identity);
-                    float randomYRotation = Random.Range(0f, 360f);
-                    spawnedPrefab.transform.rotation = Quaternion.Euler(0f, randomYRotation, 0f);
-                    
-                    switch (foodPrefabsIndex)
+                    if (currentPlantCount < maxPlants) 
                     {
-                        case 0:
-                            spawnedPrefab.transform.DOScale(0.75f, 0.75f).SetEase(Ease.OutElastic);
-                            break;
-                        case 1:
-                            spawnedPrefab.transform.DOScale(0.5f, 0.75f).SetEase(Ease.OutElastic);
-                            break;
-                        case 2:
-                            spawnedPrefab.transform.DOScale(1.2f, 0.75f).SetEase(Ease.OutElastic);
-                            break;
+                        GameObject spawnedPrefab = Instantiate(foodPrefabs[foodPrefabsIndex], point, Quaternion.identity);
+                        float randomYRotation = Random.Range(0f, 360f);
+                        spawnedPrefab.transform.rotation = Quaternion.Euler(0f, randomYRotation, 0f);
+
+                        switch (foodPrefabsIndex) 
+                        {
+                            case 0:
+                                spawnedPrefab.transform.DOScale(0.75f, 0.75f).SetEase(Ease.OutElastic);
+                                break;
+                            case 1:
+                                spawnedPrefab.transform.DOScale(0.5f, 0.75f).SetEase(Ease.OutElastic);
+                                break;
+                            case 2:
+                                spawnedPrefab.transform.DOScale(1.2f, 0.75f).SetEase(Ease.OutElastic);
+                                break;
+                        }
+
+                        leafPointManager.DecrementPoints(grassCost);
+                        TutorialManager.CompleteStep("Step_GrassSpawn");
+                        currentPlantCount++;
+                        FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerActions/GrassPlacement");
                     }
-
-                    leafPointManager.DecrementPoints(grassCost);
-                    TutorialManager.CompleteStep("Step_GrassSpawn");
-
-                    currentPlantCount++;
-
-                    FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerActions/GrassPlacement");
-                } else if (!maxFoodPopup.activeInHierarchy && gameManager.level1 && timesPopupShown < 3) {
-                    maxFoodPopup.SetActive(true);
-                    PopInAnimation(maxFoodPopup);
-                    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/PopupWarning");
-                    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/OpeningUIElement");
-                    timesPopupShown++;
+                    else if (!maxFoodPopup.activeInHierarchy && gameManager.level1 && timesPopupShown < 3) 
+                    {
+                        maxFoodPopup.SetActive(true);
+                        PopInAnimation(maxFoodPopup);
+                        timesPopupShown++;
+                        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/PopupWarning");
+                        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/OpeningUIElement");
+                    }
+                }
+                else 
+                {
+                    Debug.LogWarning("Insufficient points to buy plant.");
+                    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/CantBuy");
+                    leafPointManager.FlickerTotalPointsElement();
                 }
             }
         }
